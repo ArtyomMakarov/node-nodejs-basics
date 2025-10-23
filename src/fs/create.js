37866e55dd0d6d788d1692524bbe97d7b6d3bb5b
Artyom Makarov <Makar6031994@gmail.com>
2025-10-23 16:38:23 +0200

    feat: implement fs tasks

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import { appendFile } from "fs/promises";
+import { access, constants } from "fs";
+
 const create = async () => {
-  // Write your code here
+  access("./files/fresh.txt", constants.F_OK, async (err) => {
+    if (err) {
+      await appendFile("./files/fresh.txt", "I am fresh and young");
+
+      console.log("File created");
+    } else {
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
 await create();
