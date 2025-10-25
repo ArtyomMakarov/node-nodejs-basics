7be4323961d42bc6f6bc60d735b2ed487c3cfe9c
Artyom Makarov <Makar6031994@gmail.com>
2025-10-25 08:46:12 +0200

    feat: implement zlib tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+import { pipeline } from "stream";
+
 const compress = async () => {
-  // Write your code here
+  const source = createReadStream("./files/fileToCompress.txt");
+  const destination = createWriteStream("./files/archive.gz");
+
+  pipeline(source, createGzip(), destination, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
 };
 
 await compress();
